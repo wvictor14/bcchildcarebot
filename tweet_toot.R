8d8ab91e44d0fd84de284a1e8ba7eefff768b8bb
Victor Yuan <victor.2wy@gmail.com>
2024-06-04 23:13:26 -0700

    download and pull new vacancies

--- tweet_toot.R
+++ tweet_toot.R
@@ -1,10 +1,10 @@
 # Post vacancies to Twitter
 # Victor Yuan, June 2024
-
 library(rtweet)
 library(purrr)
 library(dplyr)
 library(readr)
+library(lubridate)
 
 # Set tokens for interacting with APIs (stored as GitHub secrets)
 twitter_token <- rtweet::rtweet_bot(
@@ -23,22 +23,54 @@ mastodon_token <- structure(
   class = "rtoot_bearer"
 ) 
 
+## read in bc child care data 
+### set dates, set timezone to pst, see OlsonNames()
+.today <- lubridate::today(tzone = 'Canada/Pacific')
+  
+url <- 'https://catalogue.data.gov.bc.ca/dataset/4cc207cc-ff03-44f8-8c5f-415af5224646/resource/9a9f14e1-03ea-4a11-936a-6e77b15eeb39/download/childcare_locations.csv'
+bccc <- readr::read_csv(url)
 
 ## identify vacancies
-## craft tweet message
-tweet_message <- c('Test')
+
+## clean up
+labels <- c('<36mo' = "VACANCY_SRVC_UNDER36",
+  "30mo-5yr" = "VACANCY_SRVC_30MOS_5YRS",
+  "Licensed Preschool" = "VACANCY_SRVC_LICPRE",
+  "Grade1-Age12" = "VACANCY_SRVC_OOS_GR1_AGE12"
+)
+.text <- bccc |>
+  #slice(1:10) |> 
+  filter(VACANCY_LAST_UPDATE == .today) |> 
+  select(NAME, CITY, PHONE, contains('VACANCY')) |> 
+  pivot_longer(
+    contains('VACANCY_SRVC'),
+    names_to = 'type',
+    names_prefix = 'VACANCY_SRVC_',
+    values_to = 'yesno'
+  ) |> 
+  filter(yesno == 'Y') |> 
+  group_by(NAME, CITY) |> 
+  summarize(.type = paste0(unique(type), collapse = ', ')) |> 
+  mutate(text = glue::glue("{NAME}, {CITY}\n\t\t{.type}"))  |>  
+  pull(text)  |> 
+  paste0(collapse = '\n') 
+ 
+if (nchar(.text) == 0) {
+  .text <- 'No new vacancies today.'
+}
+
 # Post tweet to Twitter
 possibly_post_tweet <- purrr::possibly(rtweet::post_tweet)  # will fail silently
 
 possibly_post_tweet(
-  text           = tweet_message,
+  text           = .text,
   token          = twitter_token
 )
 
 message('sending the message:')
-message(tweet_message)
+message(.text)
 
 rtoot::post_toot(
-  status = tweet_message,
+  status = .text,
   token    = mastodon_token
 )
