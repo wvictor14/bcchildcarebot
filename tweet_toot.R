d96a595dbf185d0cd60d208b126296c6dc5ef1e4
Victor Yuan <victor.2wy@gmail.com>
2024-06-08 15:44:29 -0700

    make message look better

--- tweet_toot.R
+++ tweet_toot.R
@@ -1,6 +1,5 @@
-# Post vacancies to Twitter
+# Post vacancies to mastodon @bcchildcarebot@botsin.space
 # Victor Yuan, June 2024
-library(rtweet)
 library(purrr)
 library(dplyr)
 library(readr)
@@ -11,6 +10,7 @@ library(glue)
 # parameters
 max_nchar_per_toot <- 500-10 # 10 char buffer
 max_toots <- 6
+post_title <- 'New Vacancies:'
 
 # Set tokens for interacting with APIs (stored as GitHub secrets)
 twitter_token <- rtweet::rtweet_bot(
@@ -29,23 +29,12 @@ mastodon_token <- structure(
   class = "rtoot_bearer"
 ) 
 
-## read in bc child care data 
-### set dates, set timezone to pst, see OlsonNames()
-.today <- lubridate::today(tzone = 'Canada/Pacific')
-  
+# read in bc child care data ----
 url <- 'https://catalogue.data.gov.bc.ca/dataset/4cc207cc-ff03-44f8-8c5f-415af5224646/resource/9a9f14e1-03ea-4a11-936a-6e77b15eeb39/download/childcare_locations.csv'
 bccc <- readr::read_csv(url)
 
-## identify vacancies
-
-## clean up
-labels <- c('<36mo' = "VACANCY_SRVC_UNDER36",
-  "30mo-5yr" = "VACANCY_SRVC_30MOS_5YRS",
-  "Licensed Preschool" = "VACANCY_SRVC_LICPRE",
-  "Grade1-Age12" = "VACANCY_SRVC_OOS_GR1_AGE12"
-)
-
-# vacancies for the last 7 days
+# identify vacancies ----
+## vacancies for the last 7 days
 bccc |> 
   filter(
     VACANCY_LAST_UPDATE >= .today - 7,
@@ -53,15 +42,40 @@ bccc |>
   ) |> 
   count(VACANCY_LAST_UPDATE)
 
+# craft message ----
+## set dates, set timezone to pst, see OlsonNames()
+.today <- lubridate::today(tzone = 'Canada/Pacific')
 .text <- bccc |>
-  #slice(1:10) |> 
+  
+  # filter to yesterday, and group of interest
   filter(
     VACANCY_LAST_UPDATE >= .today -1,
     VACANCY_SRVC_UNDER36 == 'Y'
   ) |> 
-  select(NAME, CITY, PHONE) |> 
+  
+  # order facilities by locations: Vancouver, Burnaby, Richmond, first
   mutate(
-    text = glue::glue("{NAME},{CITY},{PHONE}"),
+    CITY = fct(CITY) |>  fct_relevel(
+      'Vancouver', 
+      'Burnaby', 
+      'West Vancouver', 
+      'North Vancouver',
+      'Richmond'
+    )) |> 
+  arrange(CITY) |> 
+  
+  # create message 
+  
+  ## info to include
+  select(NAME, CITY, PHONE) |> 
+  mutate(text = glue::glue("{CITY}, {NAME}, {PHONE}")) |> 
+  select(text) %>%
+  
+  ## add post title
+  bind_rows(tibble(text = post_title), .) |>
+  
+  ## split by nchar limit
+  mutate(
     n_char = nchar(text),
     n_char_cumsum = cumsum(n_char),
     n_char_cut = cut(
@@ -69,26 +83,22 @@ bccc |>
       breaks = seq(0, 500*max_toots, by = max_nchar_per_toot)) |> 
       as.numeric()
     
-    ) |> 
+  ) |> 
+  
+  ## pull out text as character vector
   group_nest(n_char_cut) |> 
   mutate(text = purrr::map_chr(
     data, \(x) pull(x, text) |>  paste0(collapse = '\n')
   ),
-      text = glue::glue("{n_char_cut}/{max(n_char_cut)}\n{text}")
+  text = glue::glue("{n_char_cut}/{max(n_char_cut)}\n{text}")
   ) |> 
   pull(text)
- 
+
 if (length(.text) == 0) {
   .text <- 'No new vacancies today.'
 }
 
-# Post tweet to Twitter
-#possibly_post_tweet <- purrr::possibly(rtweet::post_tweet)  # will fail silently
-#possibly_post_tweet(
-#  text           = .text,
-#  token          = twitter_token
-#)
-
+# Toot ----
 message(glue('Sending {length(.text)} toots:'))
 .text |>  purrr::walk(message)
 
