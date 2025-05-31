7003941d921d6d428b2150fdb463e81b9c6b91c1
Victor Yuan <victor.2wy@gmail.com>
2025-05-31 14:43:24 -0700

    start js functions  to render details

--- reactable_row_details.js
+++ reactable_row_details.js
@@ -1,7 +1,27 @@
 // Custom JavaScript cell renderer for the Movie column
 function renderRowDetails(cellInfo) {
-  const header = [cellInfo.row['NAME'], cellInfo.row['SERVICE_TYPE_CD']];
   
-  const text = header.join(', ');
+  const header =  `
+    <div class= 'childcare-details-header'">
+      <h4>${cellInfo.row['NAME']}</h4>
+      <p>${cellInfo.row['SERVICE_TYPE_CD']}</p>
+    </div>
+  `;
+  
+  const details = detailsField('Last Updated', cellInfo.row['VACANCY_LAST_UPDATE']) 
+
+  const text = header + details;
+
   return text
+}
+
+
+function detailsField(name, value) {
+  
+  return `
+    <div class = 'detail-label'>
+      ${name}
+    </div>
+    <p>${value}</p>
+  `
 }
