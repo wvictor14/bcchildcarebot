0023e3af498135308fc1c9e914007777833d8514
Victor Yuan <victor.2wy@gmail.com>
2025-05-30 20:11:27 -0700

    refactor functions

---
+++ R/create_reactable.R
@@ -0,0 +1,122 @@
+
+row_details <- function(index, df) {
+  df <- df[index, ] 
+  # function that creates class based on colum name
+  df_field <- function(name, ...) {
+    if (any(is.na(...))) NULL
+    else tagList(div(class = "detail-label", name), ...)
+  }
+  
+  detail <- div(
+    class = "tbl-detail",
+    div(class = "detail-header", 
+        span(class = "detail-title", df$NAME)),
+    div(class = "detail-description", df$SERVICE_TYPE_CD),
+    
+    bscols(
+      div(
+        # column left
+        df_field('Last Updated', df$VACANCY_LAST_UPDATE),
+        df_field("Phone", df$PHONE),
+        df_field("Website", df$WEBSITE),
+        df_field("Email", df$EMAIL),
+        df_field("Address", df$ADDRESS_1),
+        df_field('Address 2', df$ADDRESS_2),
+        df_field('City', df$CITY),
+        df_field('ECE_CERTIFICATION_YN', df$ECE_CERTIFICATION_YN),
+        df_field('ELF', df$ELF_PROGRAMMING_YN),
+        df_field('IS_INCOMPLETE_IND', df$IS_INCOMPLETE_IND), 
+        df_field('IS_CCFRI_AUTH', df$IS_CCFRI_AUTH)
+      ),
+      div( 
+        # column right
+        df_field(
+          'Map', 
+          df |> 
+            leaflet() |>
+            addProviderTiles(providers$CartoDB.Voyager) |>
+            #addProviderTiles(providers$CartoDB.Positron) |>
+            addCircleMarkers(lng = ~LONGITUDE, lat = ~LATITUDE)
+        )
+      )
+    ),
+    bscols(
+      div(
+        df_field(
+          "Vacancy type",
+          reactable(
+            df |>  
+              select(contains('VACANCY_SRVC_')) |> 
+              pivot_longer(
+                everything(), 
+                names_to = 'Age Group', 
+                values_to = 'Vacancy?',
+                names_prefix = 'VACANCY_SRVC_'
+              ) |> 
+              mutate(`Vacancy?` = ifelse(!is.na(`Vacancy?`), 'Y', 'N'))
+            ,
+            pagination = FALSE,
+            defaultColDef = colDef(headerClass = "header"),
+            class = "vacancy-table",
+            theme = reactableTheme(cellPadding = "8px 12px")
+          )
+        )
+      ),
+      div(
+        df_field(
+          'Language',
+          reactable(
+            df |>  
+              select(contains('LANG_')) |> 
+              pivot_longer(everything(), names_to = 'Language', values_to = 'Y/N') |> 
+              mutate(
+                Language = str_remove_all(Language, 'LANG_') |> 
+                  str_remove_all('_YN'),
+                `Y/N` = ifelse(!is.na(`Y/N`), 'Y', 'N'))
+            ,
+            pagination = FALSE,
+            defaultColDef = colDef(headerClass = "header"),
+            class = "vacancy-table",
+            theme = reactableTheme(cellPadding = "8px 12px")
+          )
+        )
+      )
+    )
+  )
+  detail
+}
+
+create_coldefs <- function(df) {
+  # columns to hide from unested view
+  hide <- df |> 
+    select(
+      contains('VACANCY'), -VACANCY_LAST_UPDATE,
+      LONGITUDE, LATITUDE,
+      SERVICE_TYPE_CD,
+      ADDRESS_1, PHONE,
+      id, `ADDRESS_2`, WEBSITE, EMAIL,
+      
+      ECE_CERTIFICATION_YN,
+      ELF_PROGRAMMING_YN,
+      IS_INCOMPLETE_IND, IS_CCFRI_AUTH,
+      popup,
+      contains('LANG_')
+      
+    ) |> 
+    colnames() %>%
+    setNames(., .) |> 
+    map(
+      ~colDef(
+        show = FALSE
+      )
+    )
+  
+  show <- list(
+    VACANCY_LAST_UPDATE = colDef(name = 'Last Updated'),
+    NAME = colDef(name = 'Name'),
+    CITY = colDef(name = 'City')
+  )
+  
+  coldefs <- purrr::list_flatten(list(hide, show))
+  return(coldefs)
+}
---
+++ R/prepare_data.R
@@ -0,0 +1,78 @@
+prepare_data <- function(x) {
+  tbl_data <- x |>
+    
+    # filter columns
+    select(
+      SERVICE_TYPE_CD:CITY, 
+      PHONE:EMAIL,
+      LONGITUDE, LATITUDE,
+      
+      # certs
+      ECE_CERTIFICATION_YN,
+      ELF_PROGRAMMING_YN,
+      IS_INCOMPLETE_IND, IS_CCFRI_AUTH,
+      #    contains('OP_'),
+      #   contains('SRVC_'),
+      contains('LANG_'),
+      contains('VACANCY_') ) |> 
+    
+    select(NAME, everything()) |>
+    
+    
+    mutate(
+      popup = glue::glue(
+        "<b>{NAME}</b>",
+        "{SERVICE_TYPE_CD}", 
+        "{PHONE}",
+        "<br>Vacancy:", 
+        "&nbsp;&nbsp;&nbsp;&nbsp;<36 months: {VACANCY_SRVC_UNDER36}",
+        "&nbsp;&nbsp;&nbsp;&nbsp;30 months -- 5 years: {VACANCY_SRVC_30MOS_5YRS}",
+        "&nbsp;&nbsp;&nbsp;&nbsp;Preschool: {VACANCY_SRVC_LICPRE}",
+        "&nbsp;&nbsp;&nbsp;&nbsp;Grade 1 - Age 12: {VACANCY_SRVC_OOS_GR1_AGE12}",
+        .sep = "<br>"
+      )
+    ) |> 
+    
+    # crosstalk key
+    mutate(id = row_number()) |> 
+    
+    # clean vacancy and other binary columns for filters
+    
+    # if yes, replace with vacancy type, otherwise leave as NA
+    # NA will not show as a choice
+    pivot_longer(
+      cols =  c(
+        contains('VACANCY_SRVC_'), 
+        contains('LANG_'),
+        ECE_CERTIFICATION_YN,
+        ELF_PROGRAMMING_YN,
+        IS_CCFRI_AUTH,
+        IS_INCOMPLETE_IND
+      ),
+      names_to = 'name',
+      values_to = 'value'
+    ) |> 
+    mutate(
+      value = ifelse(
+        value == 'Y', 
+        str_remove_all(name, '^VACANCY_SRVC_') |> 
+          str_remove_all('^LANG_') |> 
+          str_remove_all('_YN$'),
+        NA
+      ),
+      value = case_when(
+        value == 'ECE_CERTIFICATION' ~ 'Early Childhood Educator (ECE)',
+        value == 'ELF_PROGRAMMING' ~ 'ELF',
+        value == 'IS_CCFRI_AUTH' ~ 'CCFRI authorized',
+        
+        value == '30MOS_5YRS' ~ "30 months - 5 years",
+        value == 'UNDER36' ~ 'Under 36 months',
+        value == 'LICPRE' ~ 'Licensed Pre-school',
+        value == 'OOS_GR1_AGE12' ~ 'Grade 1 - Age 12',
+        .default = value
+      )
+    ) |>
+    pivot_wider(names_from = name, values_from = value) 
+  
+  return(tbl_data)
+}
